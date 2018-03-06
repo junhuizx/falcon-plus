cb61f6b2966291033a5607b1f6a4eff3705f445c
junhuizx <junhui.zhang@newtouch.cn>
2018-03-06 15:18:19 +0800

    每个用户通过api查询告警只能获取自己的告警
    1. 修改hbs，把expression的creater信息放到结果里，这样judge才能把相
    关的信息放入到队列里面
    2. 修改alarm，把报警相关的用户放到数据库的tpl_creater里面
    3. 修改相关的api， 用户只查询出自己的报警，在alarm的cfg里面增加了
    exclusive的配置项，当为true的时候只查询自己的报警，为false的时候和
    以前一样，保持兼容。

--- modules/alarm/model/event/event_operation.go
+++ modules/alarm/model/event/event_operation.go
@@ -66,6 +66,15 @@ func InsertEvent(eve *coommonModel.Event) {
 	var errRes error
 	log.Debugf("events: %v", eve)
 	log.Debugf("expression is null: %v", eve.Expression == nil)
+	tpl_creator := ""
+	if eve.Tpl() != nil {
+		tpl_creator = eve.Tpl().Creator
+	}
+	if eve.Exp() != nil {
+		tpl_creator = eve.Exp().CreateUser
+	}
+	log.Debugf("Expression: %v", eve.Exp())
+	log.Debugf("creater: %v", tpl_creator)
 	if len(event) == 0 {
 		//create cases
 		sqltemplete := `INSERT INTO event_cases (
@@ -87,10 +96,6 @@ func InsertEvent(eve *coommonModel.Event) {
 					template_id
 					) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`
 
-		tpl_creator := ""
-		if eve.Tpl() != nil {
-			tpl_creator = eve.Tpl().Creator
-		}
 		sqlLog, errRes = q.Raw(
 			sqltemplete,
 			eve.Id,
@@ -131,11 +136,6 @@ func InsertEvent(eve *coommonModel.Event) {
 		//reopen case
 		if event[0].ProcessStatus == "resolved" || event[0].ProcessStatus == "ignored" {
 			sqltemplete = fmt.Sprintf("%v ,process_status = '%s', process_note = %d", sqltemplete, "unresolved", 0)
-		}
-
-		tpl_creator := ""
-		if eve.Tpl() != nil {
-			tpl_creator = eve.Tpl().Creator
 		}
 		if eve.CurrentStep == 1 {
 			//update start time of cases
